aa06a8edbc5ace9c02663abdf15523941fea89d6
Joshua Marquez <Joshua.marquez0718@gmail.com>
2022-11-21 16:47:23 +0800

    redirects user based on auth status

--- src/authStatus.js
+++ src/authStatus.js
@@ -1,9 +1,12 @@
 import { auth } from './config/firebaseConfig.js'
 
 auth.onAuthStateChanged(user => {
-    console.log("this is running")
     if(user){
         //USER IS LOGGED IN
+        if(document.URL.includes("login.html") || document.URL.includes("register.html") || document.URL.includes("index.html")) //USER IS LOGGED IN
+        {
+            window.location = "dashboard.html"
+        }
     }
     else{
         if(!document.URL.includes("login.html") && !document.URL.includes("register.html") && !document.URL.includes("index.html")) //USER IS NOT LOGGED IN
